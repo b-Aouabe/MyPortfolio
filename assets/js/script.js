cb28ee47970d5b2766d10cb70f21670ec88ad6fc
boubkeraouabe <boubkeraouabe@gmail.com>
2023-10-30 18:50:39 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__container img, .work__img',{interval: 200}); 
+
+
+// ======= switch mode ========
+const icon = document.querySelector('.mode');
+icon.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark')
+    document.querySelector('.l-header').classList.toggle('dark')
+    })
+
+// =====work hover =======
+const projects = document.querySelectorAll('.work__img');
+const layers = document.querySelectorAll('.layer');
+projects.forEach((pr, i) =>{
+    pr.addEventListener('mouseover', ()=>{
+        layers[i].style.opacity = 1
+    })
+})
+projects.forEach((pr, i) =>{
+    pr.addEventListener('mouseout', ()=>{
+        layers[i].style.opacity = 0
+    })
+})
